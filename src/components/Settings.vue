5372ce698568f2fcde40a9d1cf66ae84daf788b8
Dominikus Weiger <dominikus.weiger@virtual-identity.com>
2019-02-12 15:23:29 +0100

    Add: Page Components and routing example Update: Reinstall vue-cli-pwa

---
+++ src/components/Settings.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1>Settings Page</h1>
+        <p><router-link to="/">Back to home</router-link></p>
+    </div>
+</template>
---
+++ src/components/StartScreen.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <h1>This is a Startscreen</h1>
+        <p>
+            <router-link to="/">Back to home</router-link>
+        </p>
+    </div>
+</template>
