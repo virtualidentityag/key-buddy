73555dcf05a418d36587fe5676f33f8ad456a109
Patrick Müller-Klug <patrick.mueller-klug@virtual-identity.com>
2019-02-21 15:06:00 +0100

    start screen: please add css grid

--- src/views/StartScreen.vue
+++ src/views/StartScreen.vue
@@ -1,6 +1,30 @@
 <template>
-    <div>
-        <h1>This is a Startscreen</h1>
-        <p><router-link to="/">Back to home</router-link></p>
-    </div>
+  <div class="start">
+    <Bar/>
+    <Headline content="Welcome to KEY BUDDY!"/>
+  </div>
 </template>
+
+<script>
+import Headline from "../components/Headline.vue";
+import Bar from "../components/Bar.vue";
+
+export default {
+  name: "Startscreen",
+  components: {
+    Headline,
+    Bar
+  }
+};
+</script>
+
+<!-- Add 'scoped' attribute to limit CSS to this component only -->
+<style scoped>
+.start .bar {
+  margin-bottom: 10px;
+}
+.start .headline {
+  width: 250px;
+  margin: 0 auto;
+}
+</style>
