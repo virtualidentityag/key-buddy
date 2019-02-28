0cd473b96e7c6a66e8a6e67f13ee564153c8036a
Dominikus Weiger <dominikus.weiger@virtual-identity.com>
2019-02-28 16:19:23 +0100

    Add: Liveupdate People and keys in office

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,113 +1,117 @@
 <template>
-  <div class="home">
-    <Header></Header>
-	<div class="home__container container">
-		<headline 
-			content="Are you in the office right now?" 
-			headline-class="headline--topLeft"
-		/>
-		<input-toggle 
-			:checked="isPresent" 
-			v-on:toggle="togglePresence"
-		/>
-		<bar/>
-		<counter 
-			:number="keysInOffice" 
-			content="Keys are currently in the office"
-		/>
-		<counter 
-			:number="usersInOffice" 
-			content="People are currently in the office"
-		/>
-		<information-text
-			content="Make sure to check in every morning and check out every evening to ensure the system is up-to-date!"
-			information-text-class="informationText--alignLeft"
-		/>
-	</div>
-  </div>
+    <div class="home">
+        <Header></Header>
+        <div class="home__container container">
+            <headline
+                    content="Are you in the office right now?"
+                    headline-class="headline--topLeft"
+            ></headline>
+            <input-toggle
+                    :checked="isPresent"
+                    v-on:toggle="togglePresence"
+            ></input-toggle>
+            <bar></bar>
+            <counter
+                    :number="office.keysInOffice"
+                    content="Keys are currently in the office"
+            ></counter>
+            <counter
+                    :number="office.usersInOffice"
+                    content="People are currently in the office"
+            ></counter>
+            <information-text
+                    content="Make sure to check in every morning and check out every evening to ensure the system is up-to-date!"
+                    information-text-class="informationText--alignLeft"
+            ></information-text>
+        </div>
+    </div>
 </template>
 
 <script>
-import Header from '../components/Header.vue';
-import Headline from '../components/Headline.vue';
-import Bar from '../components/Bar.vue';
-import InformationText from '../components/InformationText.vue';
-import InputToggle from '../components/InputToggle.vue';
-import Counter from '../components/Counter.vue';
+    import Header from '../components/Header.vue';
+    import Headline from '../components/Headline.vue';
+    import Bar from '../components/Bar.vue';
+    import InformationText from '../components/InformationText.vue';
+    import InputToggle from '../components/InputToggle.vue';
+    import Counter from '../components/Counter.vue';
+    import {mapState} from 'vuex';
 
-export default {
-	name: 'Home',
-	components: {
-		Header,
-		Headline,
-		Bar,
-		InformationText,
-		InputToggle,
-		Counter
-	},
-	data() {
-		return {
-			performingRequest: false,
-			currentUser: {},
-			isPresent: false,
-			keysInOffice: 0,
-			usersInOffice: 0
-		};
-	},
-	created: function () {
-		this.$store.dispatch('fetchUserProfile')
-			.then(res => {
-				this.currentUser = res;
-				this.isPresent = this.currentUser.inOffice;
-			})
-		this.$store.dispatch('fetchOffice')
-			.then(res => {
-				this.keysInOffice = res.keysInOffice;
-				this.usersInOffice = res.usersInOffice;
-			})
-	},
-	methods: {
-		togglePresence() {
-			this.performingRequest = true;
-			this.isPresent = !this.isPresent;
-			this.$store.dispatch('updatePresence', this.isPresent)
-				.then(res => {
-					this.currentUser = res.user;
-					this.isPresent = res.user.inOffice;
-					this.keysInOffice = res.office.keysInOffice;
-					this.usersInOffice = res.office.usersInOffice;
-				})
-			this.performingRequest = false;
-		}
-	}
-};
+    export default {
+        name: 'Home',
+        components: {
+            Header,
+            Headline,
+            Bar,
+            InformationText,
+            InputToggle,
+            Counter
+        },
+        data() {
+            return {
+                performingRequest: false,
+                currentUser: {},
+                isPresent: false,
+                keysInOffice: 0,
+                usersInOffice: 0
+            };
+        },
+        created: function () {
+            this.$store.dispatch('fetchUserProfile')
+                .then(res => {
+                    this.currentUser = res;
+                    this.isPresent = this.currentUser.inOffice;
+                })
+            this.$store.dispatch('fetchOffice')
+                .then(res => {
+                    this.keysInOffice = res.keysInOffice;
+                    this.usersInOffice = res.usersInOffice;
+                })
+        },
+        computed: {
+            ...mapState(['office'])
+        },
+        methods: {
+            togglePresence() {
+                this.performingRequest = true;
+                this.isPresent = !this.isPresent;
+                this.$store.dispatch('updatePresence', this.isPresent)
+                    .then(res => {
+                        this.currentUser = res.user;
+                        this.isPresent = res.user.inOffice;
+                        this.keysInOffice = res.office.keysInOffice;
+                        this.usersInOffice = res.office.usersInOffice;
+                    })
+                this.performingRequest = false;
+            }
+        }
+    };
 </script>
 <style>
-.home__container {
-  padding: 15px;
-}
+    .home__container {
+        padding: 15px;
+    }
 
-.home .header {
-  grid-column: 1 / 13;
-}
+    .home .header {
+        grid-column: 1 / 13;
+    }
 
-.home .headline {
-  grid-column: 1 / 9;
-}
+    .home .headline {
+        grid-column: 1 / 9;
+    }
 
-.home .inputToggle {
-  grid-column: 9 / 13;
-}
+    .home .inputToggle {
+        grid-column: 9 / 13;
+    }
 
-.home .bar {
-  grid-column: 1 / 13;
-}
+    .home .bar {
+        grid-column: 1 / 13;
+    }
 
-.home .informationText {
-  grid-column: 1 / 13;
-}
+    .home .informationText {
+        grid-column: 1 / 13;
+    }
 
-.home .counter {
-  grid-column: 1 / 13;
-}
+    .home .counter {
+        grid-column: 1 / 13;
+    }
 </style>
