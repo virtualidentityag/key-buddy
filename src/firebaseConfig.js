aa40d06125156d5f7e690e0e1198a9f4d9170530
Stefanie Neubert <stefanie.neubert@virtual-identity.com>
2019-03-29 09:25:28 +0100

    Add messaging key to firebase config

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -7,7 +7,8 @@ const config = {
     databaseURL: "https://keybuddy-e2825.firebaseio.com",
     projectId: "keybuddy-e2825",
     storageBucket: "keybuddy-e2825.appspot.com",
-    messagingSenderId: "140535794289"
+	messagingSenderId: "140535794289",
+	publicVapidKey: "BHnsAdd4S-cSl2ABKAwZ6eRrUMjLTUj_FU8MAc-hhI8jJxl9MGVyQuRUCosCWJeilS2aO0VFK8nAJxht1yb56_U"
 };
 
 firebase.initializeApp(config);
@@ -16,13 +17,16 @@ firebase.initializeApp(config);
 const db = firebase.firestore();
 const auth = firebase.auth();
 const currentUser = auth.currentUser;
+const messaging = firebase.messaging();
+messaging.usePublicVapidKey(config.publicVapidKey);
 
 const userCollection = db.collection('user');
 const officeCollection = db.collection('office');
 
 export {
     db,
-    auth,
+	auth,
+	messaging,
     currentUser,
     userCollection,
     officeCollection
